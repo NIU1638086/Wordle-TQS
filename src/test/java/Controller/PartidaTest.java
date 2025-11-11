0626ae249267ab81a89a81416834f6a4b3a9eeac
albay <1638086@uab.cat>
2025-11-11 14:04:37 +0100

    commit test controller

---
+++ src/test/java/Controller/PartidaTest.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PartidaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
